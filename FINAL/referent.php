b513e3f09b3dba320b472cce1c153a7b84ecf73e
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2023-06-11 15:13:50 +0200

    Update referent.php

--- FINAL/referent.php
+++ FINAL/referent.php
@@ -561,5 +561,20 @@
             </div>
         </div>
     </div>
+    <script>
+        var checks = document.querySelectorAll(".check");
+
+        var max = 4;
+
+        for (var i = 0; i < checks.length; i++) {
+            checks[i].onclick = selectiveCheck;
+        }
+
+        function selectiveCheck(event) {
+            var checkedChecks = document.querySelectorAll(".check:checked");
+            if (checkedChecks.length >= max + 1)
+                return false;
+        }
+    </script>
 </body>
 </html>
