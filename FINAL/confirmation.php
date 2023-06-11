c4cdf049e86844a5b67cfa58773d1b58b60afc1f
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2023-06-11 15:53:39 +0200

    Add files via upload

---
+++ FINAL/confirmation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Référent</title>
+    <link rel="stylesheet" type="text/css" href="referent.css">
+</head>
+<body>
+    <header>
+        <img src="./logo2.png" id="logo" alt="Logo">
+        <div style="flex-direction: column; justify-content: center; align-items: flex-end; flex: 3 1 auto; display: flex;">
+            <div id="title">RÉFÉRENT</div>
+            <div id="subtitle">Je confirme la valeur de ton engagement</div>
+        </div>
+    </header>
+
+    <div class="navbar">
+        <ul>
+            <li><a id="jeune" href="#">JEUNE</a></li>
+            <li><a id="referent" href="#">RÉFÉRENT</a></li>
+            <li><a id="consultant" href="#">CONSULTANT</a></li>
+            <li><a id="partenaires" href="#">PARTENAIRES</a></li>
+        </ul>
+    </div>
+
+    <div class="title">Validez cet engagement en prenant en compte sa valeur</div>
+
+
+<div class="title">Merci d'avoir confirmer cette reference</div>
+<img style="display: block; margin: 0 auto;" src="checkmark.gif" alt="Animated GIF">
+</body>
+</html>
